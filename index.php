3a615e074d39dabbd107c8f8f93c2dd82048e7f2
amedeopro <amedeopro@me.com>
2019-02-10 15:29:56 +0100

    partials

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="it" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>La prima CRUD</title>
-    <link rel="stylesheet" href="css/app.css">
-  </head>
-  <body>
-    <div class="container">
-      Ciao
-    </div>
-  </body>
-</html>
+
+
+<?php include "partials/_head.php";  ?>
+<?php include "partials/_navbar.php"; ?>
+<?php include "partials/_footer.php"; ?>
---
+++ partials/_head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>La prima CRUD</title>
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  <body>
+    
